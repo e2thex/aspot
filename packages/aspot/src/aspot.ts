c36ebea132d5ad61d0e95d0ba4600522ade1a32f
Summerfield, Erik [CPCUS Non-J&J] <esummer3@ITS.JNJ.com>
2021-09-30 23:05:13 -0500

    updates scripts and add some type exports

--- packages/aspot/src/aspot.ts
+++ packages/aspot/src/aspot.ts
@@ -1,7 +1,10 @@
-import basicStoreNode from "./basicStoreNode";
+import basicStoreNode, { TermType, StoreNode } from "./basicStoreNode";
 import localNode from "./localNode";
 import rootNode from "./rootNode";
 export * from './match';
+import { PredicateNode } from "./predicateNode";
+import { SubjectNode } from "./subjectNode";
+
 
 
 
@@ -10,4 +13,8 @@ const aspotLocal = (name:string) => rootNode(localNode(name)(basicStoreNode()));
 export {
   aspot,
   aspotLocal,
+  TermType,
+  PredicateNode,
+  SubjectNode,
+  StoreNode,
 }
