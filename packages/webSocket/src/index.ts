32d205427d4051c759846f8a244d77927d2b8bc9
Summerfield, Erik [CPCUS Non-J&J] <esummer3@ITS.JNJ.com>
2021-10-13 09:27:24 -0500

    did some work to stop the pushing of data around all the time

--- packages/webSocket/src/index.ts
+++ packages/webSocket/src/index.ts
@@ -1,26 +1,26 @@
 import { Sentence, StoreNode } from '@aspot/core';
 import { MD5 } from 'crypto-js';
+const hash = (sentence:Sentence) => MD5(JSON.stringify(sentence)).toString() 
 const webSocketConnectorCore = (deps) => (url:string, group:string) => (node:StoreNode) => {
   const {} = deps;
   if(typeof window !== 'undefined') {
     const recentSentences = []
     const onUpdate = (sentence:Sentence) => {
-      recentSentences.push(MD5(JSON.stringify(sentence)))
+      recentSentences.push(hash(sentence))
       node.set(sentence);
     }
     const ws = new WebSocket(url);
     const ping = () => {
       ws.send(JSON.stringify({action:'ping'}));
-      // Why we are doing the ping we are going to hackaly do some clean up on recentSentences
-      recentSentences.shift();
-      window.setTimeout(ping, 2000);
+      // While we are doing the ping we are going ro do some clean up on recentSentences so it does not get to big
+      recentSentences.splice(0, Math.floor(recentSentences.length/2));
+      window.setTimeout(ping, 60000);
     }
     ws.addEventListener('open', function (event) {
       ws.send(JSON.stringify({action:'join', group}));
       ws.send(JSON.stringify({action:'requestUpdates', group}));
       ws.addEventListener('message', (event) => {
         const {action, ...data } = JSON.parse(event.data);
-        console.log(data);
         if(action === 'update') {
           data.sentences.forEach(onUpdate);
         }
@@ -31,7 +31,7 @@ const webSocketConnectorCore = (deps) => (url:string, group:string) => (node:Sto
       ping();
     });
     const sendUpdate = (...sentences:Sentence[]) => {
-      const newSentences = sentences.filter(s => !recentSentences.includes(MD5(JSON.stringify(s))))
+      const newSentences = sentences.filter(s => !recentSentences.includes(hash(s)))
       if(newSentences.length > 0) {
         const pack = {action:'update', group, sentences:newSentences}
         ws.send(JSON.stringify(pack))
